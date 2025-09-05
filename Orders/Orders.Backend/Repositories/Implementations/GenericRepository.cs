562309ca06aeb80d5d2955f726de1ea1e0b7b7c9
MidnightGhostmare <juanpa20037@gmail.com>
2025-09-05 03:46:10 -0500

    Add -Category clase- -Repository + Unit of work

---
+++ Orders/Orders.Backend/Repositories/Implementations/GenericRepository.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Backend.Data;
+using Orders.Backend.Repositories.Interfaces;
+using Orders.Share.Responses;
+
+namespace Orders.Backend.Repositories.Implementations;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    private readonly DataContext _context;
+    private readonly DbSet<T> _entity; //Representa la entidad(tabla) en la base de datos
+
+    public GenericRepository(DataContext context, DbSet<T> entity)
+    {
+        _context = context;
+        _entity = entity;
+    }
+
+    public virtual async Task<ActionResponse<T>> AddAsync(T entity)
+    {
+        try
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return new ActionResponse<T>
+            {
+                WasSuccess = true,
+                Result = entity
+            };
+        }
+        catch (DbUpdateException)
+        {
+            return DbUpdateExceptionActionResponse();//Si falla es porque ya existe
+        }
+        catch (Exception ex2)
+        {
+            return ExeptionActionResponse(ex2);
+        }
+    }
+
+    public virtual async Task<ActionResponse<T>> DeleteAsync(int id)
+    {
+        var row = await _entity.FindAsync(id);
+        if (row == null)
+        {
+            return new ActionResponse<T>
+            {
+                Message = "No se encontro el registro."
+            };
+        }
+
+        try
+        {
+            _entity.Remove(row);
+            await _context.SaveChangesAsync();
+            return new ActionResponse<T>
+            {
+                WasSuccess = true
+            };
+        }
+        catch
+        {
+            return new ActionResponse<T>
+            {
+                Message = "No se pudo eliminar porque tiene registros asociados."
+            };
+        }
+    }
+
+    public virtual async Task<ActionResponse<T>> GetAsync(int id)
+    {
+        var row = await _entity.FindAsync(id);
+        if (row == null)
+        {
+            return new ActionResponse<T>
+            {
+                Message = "No se encontro el registro."
+            };
+        }
+        return new ActionResponse<T>
+        {
+            WasSuccess = true,
+            Result = row
+        };
+    }
+
+    public virtual async Task<ActionResponse<IEnumerable<T>>> GetAsync()
+        => new ActionResponse<IEnumerable<T>>
+        {
+            WasSuccess = true,
+            Result = await _entity.ToListAsync()
+        };
+
+    public virtual async Task<ActionResponse<T>> UpdateAsync(T entity)
+    {
+        try
+        {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+            return new ActionResponse<T>
+            {
+                WasSuccess = true,
+                Result = entity
+            };
+        }
+        catch (DbUpdateException)
+        {
+            return DbUpdateExceptionActionResponse();//Si falla es porque ya existe
+        }
+        catch (Exception ex2)
+        {
+            return ExeptionActionResponse(ex2);
+        }
+    }
+
+    private ActionResponse<T> ExeptionActionResponse(Exception ex2)
+        => new ActionResponse<T> { Message = ex2.Message };
+
+    private ActionResponse<T> DbUpdateExceptionActionResponse()
+        => new ActionResponse<T> { Message = "Ya existe el registro." };
+}
---
+++ Orders/Orders.Share/Responses/ActionResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Orders.Share.Responses;
+
+public class ActionResponse<T>
+{
+    public bool WasSuccess { get; set; }
+    public T? Result { get; set; }
+    public string? Message { get; set; }
+}
