98f074c1962eb8ae2b66d4e840bcea3adc5a2b9d
MidnightGhostmare <juanpa20037@gmail.com>
2025-11-04 22:50:38 -0500

    Release: -Add Security Roles in Frontend

--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor.Services;
+using Orders.Frontend.AuthenticationProviders;
 using Orders.Frontend.Components;
 using Orders.Frontend.Repositories;
 
@@ -8,7 +10,9 @@ builder.Services.AddMudServices();
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("http://localhost:5071") });
+builder.Services.AddSingleton(_ => new HttpClient { BaseAddress = new Uri("http://localhost:5071") });
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
 builder.Services.AddScoped<IRepository, Repository>();
 
 var app = builder.Build();
